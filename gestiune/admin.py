087c6102eb67f9072f75d441ae2a72027c0a5bd7
AurelianAM <117901747+AurelianAM@users.noreply.github.com>
2024-11-24 12:09:04 +0200

    post_delete behavior

--- gestiune/admin.py
+++ gestiune/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import FondRate, Incasare
+from .models import FondRate, Incasare, IncasareDiversa, Cheltuiala
 
 # Register your models here.
 
 admin.site.register(FondRate)
 admin.site.register(Incasare)
+admin.site.register(Cheltuiala)
+admin.site.register(IncasareDiversa)
--- gestiune/models.py
+++ gestiune/models.py
@@ -27,6 +27,28 @@ class Incasare(models.Model):
     def __str__(self):
         return f'{self.elev.nume} {self.elev.prenume} a platit {self.suma} lei procesat = {self.is_applied}'
 
+class IncasareDiversa(models.Model):
+    id = models.AutoField(primary_key=True)
+    clasa = models.ForeignKey(Clasa, on_delete=models.SET_NULL, null=True)
+    suma = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    detalii = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    is_applied = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.created} - {self.suma} lei - {self.detalii} = {self.is_applied}'
+
+class Cheltuiala(models.Model):
+    id = models.AutoField(primary_key=True)
+    clasa = models.ForeignKey(Clasa, on_delete=models.SET_NULL, null=True)
+    suma = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    detalii = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    is_applied = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.created} - {self.suma} lei - {self.detalii} = {self.is_applied}'
+
 def fondRateAfterSave(sender, instance, created, **kwargs):
     if instance.is_active and not instance.is_applied:
         totiElevii = Elev.objects.filter(clasa = instance.clasa)
@@ -36,6 +58,14 @@ def fondRateAfterSave(sender, instance, created, **kwargs):
                 elev.save()
         instance.is_applied = True
         instance.save()
+
+def fondRateAfterDelete(sender, instance, **kwargs):
+    if instance.is_active and instance.is_applied:
+        totiElevii = Elev.objects.filter(clasa = instance.clasa)
+        if totiElevii is not None:
+            for elev in totiElevii:
+                elev.restDePlata -= instance.suma
+                elev.save()
 
 def incasareAfterSave(sender, instance, created, **kwargs):
     if not instance.is_applied:
@@ -49,5 +79,53 @@ def incasareAfterSave(sender, instance, created, **kwargs):
         instance.is_applied = True
         instance.save()
 
+def incasareAfterDelete(sender, instance, **kwargs):
+    if instance.is_applied:
+        elev = instance.elev
+        clasa = elev.clasa
+        clasa.fond -= instance.suma
+        clasa.save()
+        elev.restDePlata += instance.suma
+        elev.contributie -= instance.suma
+        elev.save()
+
+def cheltuialaAfterSave(sender, instance, created, **kwargs):
+    if not instance.is_applied:
+        clasa = instance.clasa
+        clasa.fond -= instance.suma
+        clasa.save()
+        instance.is_applied = True
+        instance.save()
+
+def cheltuialaAfterDelete(sender, instance, **kwargs):
+    if instance.is_applied:
+        clasa = instance.clasa
+        clasa.fond += instance.suma
+        clasa.save()
+
+def incasareDiversaAfterSave(sender, instance, created, **kwargs):
+    if not instance.is_applied:
+        clasa = instance.clasa
+        clasa.fond += instance.suma
+        clasa.save()
+        instance.is_applied = True
+        instance.save()
+
+
+def incasareDiversaAfterDelete(sender, instance, **kwargs):
+    if instance.is_applied:
+        clasa = instance.clasa
+        clasa.fond -= instance.suma
+        clasa.save()
+
 models.signals.post_save.connect(fondRateAfterSave, sender=FondRate)
+models.signals.post_delete.connect(fondRateAfterDelete, sender=FondRate)
+
 models.signals.post_save.connect(incasareAfterSave, sender=Incasare)
+models.signals.post_delete.connect(incasareAfterDelete, sender=Incasare)
+
+models.signals.post_save.connect(cheltuialaAfterSave, sender=Cheltuiala)
+models.signals.post_delete.connect(cheltuialaAfterDelete, sender=Cheltuiala)
+
+models.signals.post_save.connect(incasareDiversaAfterSave, sender=IncasareDiversa)
+models.signals.post_delete.connect(incasareDiversaAfterDelete, sender=IncasareDiversa)
