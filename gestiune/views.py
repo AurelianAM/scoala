dbd35dd3d4331df596b7a0c378e06fa48686dd02
AurelianAM <anescuaurelian@gmail.com>
2024-12-01 11:13:45 +0200

    updated home

--- gestiune/views.py
+++ gestiune/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from scoala.models import Clasa
 
 # Create your views here.
 
 def homeView(request):
-    return render(request, 'home.html')
+    context = {}
+    if request.method == "GET":
+        toateClasele = Clasa.objects.all()
+        context.update({'toateClasele' : toateClasele})
+    
+    return render(request, 'home.html', context=context)
